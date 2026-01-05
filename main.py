e95fe38ed397ea8bd317e09e6a8d77782cb202b5
moeez-siddiqui <114137831+moeez-siddiqui@users.noreply.github.com>
2026-01-05 10:04:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://deen-daily.netlify.app"
+
         # "https://your-netlify-domain.netlify.app",
     ],
     allow_credentials=True,
